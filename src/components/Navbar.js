5802d17d394631d247580e8e18e57db91c7f1cb2
gill-jaskiran <jaskirangill14@gmail.com>
2025-06-24 22:13:08 -0400

    updates - commeting a page out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
                 <li><Link to="/cover-letter">Cover Letter</Link></li>
                 <li><Link to="/credentials">Credentials</Link></li>
                 <li><Link to="/work-experience">Work Samples</Link></li>
-                <li><Link to="/summary">Summary</Link></li>
+                {/* <li><Link to="/summary">Summary</Link></li> */}
             </ul>
         </nav>
     );
